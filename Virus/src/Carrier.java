9659b492af943a6459f85421395a624fbac8ab83
irmiev <irmiev@gmail.com>
2015-01-23 13:15:10 +0000

    Renaming the package

--- Virus/src/Carrier.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Graphics;
-
-
-public interface Carrier {
-	public void setAlive(boolean alive);
-	public boolean getAlive();
-	public boolean isInfected();
-	public void paint(Graphics g);
-}
